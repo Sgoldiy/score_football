80fce01c7cdb8873865397f394ec9c0a94406498
SAHIL <sahilgondaliyar@gmail.com>
2026-07-02 18:06:51 +0530

    new avatar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.footballpluse.footballapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.footballpluse.footballapp"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
